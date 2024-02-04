f3f57e49c0b6bd84b79fe0ace1591e990b8330dd
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-04 13:18:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
+import React, { Component } from "react";
+import Countdown from "./Countdown.js";
+import logo from "./logo.png";
+
+
+class App extends Component {
+  render() {
+    const currentDate = new Date();
+    const year =
+      currentDate.getMonth() === 11 && currentDate.getDate() > 23
+        ? currentDate.getFullYear() + 1
+        : currentDate.getFullYear();
+    return (
+      <div className="App">
+        <div className="App-header">
+          ### <img src={logo} className="App-logo" alt="logo" /> ###
+          <h2>Countdown to Christmas 2024</h2>
+
+        </div>
+
+        <h3 className="title">
+          Christmas Eve is coming soon (Midnight of 23rd to 24th Dec, UTC time):
+        </h3>
+        <Countdown date={`${year}-12-24T00:00:00`} />
+      </div>
+    );
+  }
+}
+
+export default App;
 
