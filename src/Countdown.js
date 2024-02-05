b83e9a11dcfb8df34b6860dae5a7ffeb8dec01a8
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-05 11:52:37 +0100

    Update Countdown.js
    -Added diff 86400, 3600, 60. 
    -return timeLeft();

--- src/Countdown.js
+++ src/Countdown.js
@@ -44,3 +44,22 @@ class Countdown extends Component {
       timeLeft.years = Math.floor(diff / (365.25 * 86400));
       diff -= timeLeft.years * 365.25 * 86400;
     }
+
+     if (diff >= 86400) {
+      // 24 * 60 * 60
+      timeLeft.days = Math.floor(diff / 86400);
+      diff -= timeLeft.days * 86400;
+    }
+    if (diff >= 3600) {
+      // 60 * 60
+      timeLeft.hours = Math.floor(diff / 3600);
+      diff -= timeLeft.hours * 3600;
+    }
+    if (diff >= 60) {
+      timeLeft.min = Math.floor(diff / 60);
+      diff -= timeLeft.min * 60;
+    }
+    timeLeft.sec = diff;
+
+    return timeLeft;
+  }
