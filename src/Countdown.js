b241ecbcef0581e306ad613777cbc97492d410e5
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-05 11:34:57 +0100

    Update Countdown.js

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,1 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
+class Countdown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      days: 0,
+      hours: 0,
+      min: 0,
+      sec: 0,
+    };
+  }
